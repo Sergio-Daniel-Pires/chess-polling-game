501dc6c7bf62e30e28d9c14e1a160d9074343527
Sergio Pires <sergiodanpires@gmail.com>
2024-07-19 20:15:23 -0300

    Change ports from XXX0 to XXX1

--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -6,6 +6,6 @@ FLASK_ENV = os.environ.get("FLASK_ENV", "development")
 RECAPTCHA_SECRET_KEY = os.environ["RECAPTCHA_SECRET_KEY"]
 
 # Gunicorn config
-GUNICORN_BIND = os.environ.get("GUNICORN_BIND", "0.0.0.0:5000")
+GUNICORN_BIND = os.environ.get("GUNICORN_BIND", "0.0.0.0:5001")
 GUNICORN_WORKERS = os.environ.get("GUNICORN_WORKERS", 4)
 GUNICORN_TIMEOUT = os.environ.get("GUNICORN_TIMEOUT", 120)
