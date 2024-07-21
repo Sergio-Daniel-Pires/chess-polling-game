2a1bed38b1682717ac75597dd5a0a2673d286866
root <root@vmi1889434.contaboserver.net>
2024-07-21 03:37:58 +0200

    Reset daily with half time hotfix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -53,8 +53,8 @@ def create_app() -> Flask:
     cors.init_app(app)
 
     # Create chess games
-    chess_games.add_game("Daily", SECONDS_IN_DAY)
-    chess_games.add_game("6 Hours", SECONDS_IN_HOUR * 6)
+    chess_games.add_game("Daily", SECONDS_IN_DAY, next_update=(SECONDS_IN_DAY//2))
+    # chess_games.add_game("6 Hours", SECONDS_IN_HOUR * 6)
 
     scheduler.add_job(
         chess_games.verify_games_to_update, "interval",
